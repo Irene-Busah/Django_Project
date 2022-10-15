d89589900cc6d912f57f0d7ec60644cc0e7b5c3f
Irene-Busah <i.busah@alustudent.com>
2022-10-15 23:54:12 +0200

    remove secret key

--- songcrud/songcrud/settings.py
+++ songcrud/songcrud/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%htt4valvzd%kgy3tz%4^ixi*dzi#ow17)9&$e@ux)e#f92c%0'
+SECRET_KEY = 'Check gitignore'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
